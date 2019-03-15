84b061f5ff03cbb6cd1078416a9509680eb78a9f
jo3l17 <joelvaldezangeles@gmail.com>
2019-03-15 14:08:43 -0500

    proyecto scrum semana 4

---
+++ Codigo3/Semana4/Scrum/ConsoleApp1/ConsoleApp1/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Cliente
+    {
+        string _DNI;
+        string _nombre;
+        string _apellido;
+        Cuenta miscuentas;
+
+        public Cliente(string DNI, string nombre, string apellido)
+        {
+            _DNI = DNI;
+            _nombre = nombre;
+            _apellido = apellido;
+        }
+
+        public string DNI { get => _DNI; set => _DNI = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Apellido { get => _apellido; set => _apellido = value; }
+    }
+}
