d95205c279240556fdfc758bcd8c5718a2361646
jo3l17 <joelvaldezangeles@gmail.com>
2019-03-20 17:55:49 -0500

    avance semana 3 dia 3
    bootstrap
    diagramas en staruml

--- Codigo3/Semana5/Dia1/factura.js
+++ Codigo3/Semana5/Dia1/factura.js
@@ -1,5 +1,5 @@
-window.addEventListener("load",function(){
-    
+window.addEventListener("load", function () {
+
     // let factura = {
     //     nombre: "nombre",
     //     RUC:"RUC",
@@ -15,28 +15,48 @@ window.addEventListener("load",function(){
     var totalFinal = document.getElementById("totalFinal")
     var tot = 0;
     var cont = 1;
-    
+
     let factura = {
         nombre: "nombre",
-        RUC:"RUC",
-        fecha:"fecha",
-        productos:[]
+        RUC: "RUC",
+        fecha: "fecha",
+        productos: []
+    }
+    function construirFactura(){
+        let stringFacturaStorage = localStorage.getItem("factura");
+        if(stringFacturaStorage != null){
+            let jsonFacturaStorage = JSON.parse(stringFacturaStorage);
+            factura = jsonFacturaStorage;
+            dibujarTablaConStorage();
+        }else{
+            console.log("no habia storage");
+        }
+    }
+
+    function dibujarTablaConStorage(){
+        for (let i = 0; i < factura.productos.Length; i++) {
+            let tr = document.createElement("tr");
+            let tdNum = document.createElement("td")
+            tdNum.innerHTML = i+1;
+            let tdCant = document.createElement("td");
+            tdCant.innerHTML = productos[i].cant;
+            let tdDesc = document.createElement("td");
+            tdDesc.innerHTML = productos[i].desc;
+            let tdPunit = document.createElement("td");
+            tdPunit = productos[i].punit;
+
+            tr.appendChild(tdNum);
+            tr.appendChild(tdCant);
+            tr.appendChild(tdDesc);
+            tr.appendChild(tdPunit);
+
+            miBody.appendChild(tr);
+        }
     }
     var agregar = document.getElementById("btnAgregar");
-    agregar.addEventListener("click",function(event){
+    agregar.addEventListener("click", function (event) {
 
-        event.preventDefault();
-
-
-
-        let productos ={
-            cant:"",
-            desc:"",
-            punit:"",
-            categoria:""
-        }
-
-        if (cont==1) {
+        if (cont == 1) {
             factura.nombre = document.getElementById("inputNombre").value;
             factura.RUC = document.getElementById("inputRUC").value;
             factura.fecha = document.getElementById("inputFecha").value;
@@ -47,42 +67,57 @@ window.addEventListener("load",function(){
         var inputPunit = document.getElementById("inputPunit");
         var inputCategoria = document.getElementById("inputCategoria");
 
+        if (document.getElementById("inputNombre").value == ""
+            || document.getElementById("inputRUC").value == ""
+            || document.getElementById("inputFecha").value == ""
+            || inputCant.value == ""
+            || inputDesc.value == ""
+            || inputPunit.value == ""
+            || inputCategoria.value == "") {
+            alert("no ha ingresado datos");
+        } else {
 
-        productos.cant = inputCant.value;
-        productos.desc = inputDesc.value;
-        productos.punit = inputPunit.value;
-        productos.categoria = inputCategoria.value;
-        factura.productos.push(productos)
+            let productos = {
+                cant: inputCant.value,
+                desc: inputDesc.value,
+                punit: inputPunit.value,
+                categoria: inputCategoria.value
+            }
 
-        let miBody = document.getElementById("miBody");
-        let tr = document.createElement("tr");
-        
-        let tdNum = document.createElement("td")
-        let tdCant = document.createElement("td");
-        let tdDesc = document.createElement("td");
-        let tdPunit = document.createElement("td");
-        // let tdCategoria = document.createElement("td");
-        let tdSubtotal = document.createElement("td");
+            // productos.cant = inputCant.value;
+            // productos.desc = inputDesc.value;
+            // productos.punit = inputPunit.value;
+            // productos.categoria = inputCategoria.value;
+            factura.productos.push(productos);
 
-        tdNum.innerHTML = cont;
-        tdCant.innerHTML = inputCant.value;
-        tdDesc.innerHTML = inputDesc.value;
-        tdPunit.innerHTML = inputPunit.value;
-        // tdCategoria.innerHTML = inputCategoria.value;
-        tdSubtotal.innerHTML = (inputCant.value)*(inputPunit.value);
-        tot += +tdSubtotal.innerHTML;
-        totalFinal.innerHTML = tot;
-        
-        tr.appendChild(tdNum);
-        tr.appendChild(tdCant);
-        tr.appendChild(tdDesc);
-        tr.appendChild(tdPunit);
-        // tr.appendChild(tdCategoria);
-        tr.appendChild(tdSubtotal);
-        miBody.appendChild(tr);
-        cont++;
+            let miBody = document.getElementById("miBody");
+            let tr = document.createElement("tr");
 
-        let facturaString = JSON.stringify(factura);
-        localStorage.setItem("factura",facturaString);
-    })
+            let tdNum = document.createElement("td")
+            let tdCant = document.createElement("td");
+            let tdDesc = document.createElement("td");
+            let tdPunit = document.createElement("td");
+            let tdSubtotal = document.createElement("td");
+
+            tdNum.innerHTML = cont;
+            tdCant.innerHTML = inputCant.value;
+            tdDesc.innerHTML = inputDesc.value;
+            tdPunit.innerHTML = inputPunit.value;
+            tdSubtotal.innerHTML = (inputCant.value) * (inputPunit.value);
+            tot += +tdSubtotal.innerHTML;
+            totalFinal.innerHTML = tot;
+
+            tr.appendChild(tdNum);
+            tr.appendChild(tdCant);
+            tr.appendChild(tdDesc);
+            tr.appendChild(tdPunit);
+            tr.appendChild(tdSubtotal);
+            miBody.appendChild(tr);
+            cont++;
+
+            let facturaString = JSON.stringify(factura);
+            localStorage.setItem("factura", facturaString);
+        }
+    });
+    construirFactura();
 });
