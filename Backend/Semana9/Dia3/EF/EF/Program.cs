7e6421717ea5024e44a4365fbf342b530f752bee
jo3l17 <joelvaldezangeles@gmail.com>
2019-04-17 17:45:57 -0500

    entity framework script database banco

---
+++ Backend/Semana9/Dia3/EF/EF/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            parqueoEntities context = new parqueoEntities();
+            tabla usuario = new tabla
+            {
+                nombre="aasd"
+            };
+            context.tablas.Add(usuario);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Backend/Semana9/Dia3/EntityFramework/EntityFramework/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ejemploEntities context = new ejemploEntities();
+            /*customer usuario = new customer
+            {
+                idCliente = 5,
+                Name = "Claudia"
+
+            };
+            context.customers.Add(usuario);
+            context.SaveChanges();*/
+            foreach (var item in context.customers)
+            {
+                Console.WriteLine($"Cliente {item.Name}");
+            }
+            Console.WriteLine($"Cliente {context.customers.Find(1).Name}");
+
+            try
+            {
+                customer usuario1 = context.customers.FirstOrDefault(item => item.Name == "Eduardo");
+                Console.WriteLine($"Cliente {usuario1.Name}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("no se encotro el usuario");
+            }
+
+            customer unUsuario = context.customers.Find(5);
+            unUsuario.Name = "Nose";
+            context.SaveChanges();
+
+            context.customers.Remove(unUsuario);
+            context.SaveChanges();
+        }
+    }
+}
